5aca16eb57f2f8535d6c005670b493f5f0431cbb
Yasaman Parhizkar <yasaman.parhizkar@gmail.com>
2026-07-15 18:41:51 -0400

    set up websocekt

--- backend/src/api/views.py
+++ backend/src/api/views.py
@@ -1,21 +1,116 @@
-from fastapi import APIRouter
+import logging
+from fastapi import APIRouter, HTTPException, status, WebSocket, WebSocketDisconnect
 
-from ..db.client import MongoDBClient
-from .models import StartGame, Game
+from .models import StartGame, Game, get_model_safe, MoveInput
+from .crud import (
+    start_new_game,
+    list_games_from_db,
+    get_game_by_id,
+    delete_games_from_db,
+    join_new_game,
+    update_game,
+)
+from ..api.fields import PyObjectId
+from .websocket import connection_manager
+from .shortcuts import make_move
+from .validators import validate
+
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix="/games", tags=["Games"])
 
 
 @router.post("/")
-async def start_new_game(player_data: StartGame) -> Game:
-    data = {"player1": player_data.player, "player2": player_data.player}
+async def start_game(player_data: StartGame) -> Game:
+    game = await start_new_game(player_data.player)
+    if game is None:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Game cannot be started right now, please try again later",
+        )
+    return game
 
-    client = MongoDBClient()
-    inserted_result = await client.insert(Game, data)  # type: ignore[arg-type]
-    game_data = await client.get(
-        Game,  # type: ignore[arg-type]
-        inserted_result.inserted_id,  # type: ignore[arg-type]
-    )
-    result = Game(**game_data)
 
-    return result
+@router.get("/")
+async def list_games() -> list[Game]:
+    return await list_games_from_db()  # type: ignore[return-value]
+
+
+@router.get("/{game_id}/")
+async def get_game(game_id: PyObjectId) -> Game:
+    game = await get_game_by_id(game_id)
+    if game is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Game not found"
+        )
+    return game
+
+
+@router.delete("/")
+async def delete_games() -> dict[str, int]:
+    deleted_count = await delete_games_from_db()
+    return {"deleted_count": deleted_count}
+
+
+@router.post("/{game_id}/join/")
+async def join_game(game_id: PyObjectId, player_data: StartGame) -> Game:
+    game = await get_game_by_id(id=game_id)
+    if game is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Game not found"
+        )
+
+    if game.player2 is not None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Game already started",
+        )
+
+    updated_game = await join_new_game(game, player_data.player)
+    if updated_game is None:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Game cannot be joined right now, please try again later",
+        )
+
+    # notify the first player about joining
+    await connection_manager.broadcast_game(updated_game)
+
+    return updated_game
+
+
+@router.websocket("/ws/{game_id}/")
+async def websocket_game_endpoint(websocket: WebSocket, game_id: PyObjectId) -> None:
+    await connection_manager.connect(websocket, game_id)
+
+    try:
+        while True:
+            move_data = await websocket.receive_json()  # {"col": int}
+
+            # get game and move
+            game = await get_game_by_id(id=game_id)
+            move = get_model_safe(MoveInput, move_data)
+
+            # validate
+            error_msg = validate(game, move)  # type: ignore[arg-type]
+            if error_msg:
+                logger.warning(error_msg)
+                continue
+
+            # make move
+            # game = cast(Game, game)
+            # move = cast(MoveInput, move)
+            # make_move(game, move.col)  # type: ignore[arg-type]
+            make_move(game, move.col)
+
+            # update DB
+            # updated_game = cast(
+            #     Game, await update_game(game.id, game.model_dump())
+            # )
+            updated_game = await update_game(game.id, game.model_dump())
+
+            # broadcast game to all players
+            await connection_manager.broadcast_game(updated_game)
+    except WebSocketDisconnect:
+        connection_manager.disconnect(websocket, game_id)
--- backend/tests/test_core.py
+++ backend/tests/test_core.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.core import is_valid_move, detect_winner, mark_winner
+from src.core import is_valid_move, detect_winner, mark_winner, calculate_row_by_col
 
 
 @pytest.mark.parametrize(
@@ -310,3 +310,27 @@ def test_detect_winner(board, expected):
 def test_mark_winner(board, winner, expected):
     mark_winner(board, winner)
     assert board == expected
+
+
+@pytest.mark.parametrize(
+    "col, expected",
+    (
+        (0, 5),
+        (1, 3),
+        (2, 2),
+        (3, 5),
+        (4, None),
+        (5, 2),
+        (6, 1),
+    ),
+)
+def test_calculate_row_by_col(col, expected):
+    board = [
+        [0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 2, 0, 0],
+        [0, 0, 0, 0, 1, 0, 2],
+        [0, 0, 1, 0, 1, 1, 2],
+        [0, 2, 2, 0, 2, 2, 1],
+        [0, 1, 1, 0, 1, 1, 1],
+    ]
+    assert calculate_row_by_col(board, col) == expected
